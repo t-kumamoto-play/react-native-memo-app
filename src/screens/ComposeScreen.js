48862c09dd1b62c5124a8757cc2a80dda52ba185
tsuyoshi.kumamoto <tsuyoshi.kumamoto@play.jp>
2024-02-01 19:13:17 +0900

    メモ作成画面の中身作成

--- src/screens/ComposeScreen.js
+++ src/screens/ComposeScreen.js
@@ -2,21 +2,44 @@ import React, { useState } from "react";
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform
 } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 export default function ComposeScreen() {
+  const [text, setText] = useState("");
+  const onPressSave = (e) => {
+    // TODO
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>メモ作成画面</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>メモ作成画面</Text>
+    // </View>
+
+    <KeyboardAvoidingView style={styles.container}>
+      <TextInput
+        style={{ marginBottom: 16 }}
+        mode="outlined"
+        placeholder="メモを入力してください"
+        multiline
+        onChangeText={(text) => setText(text)}
+      />
+      <Button
+        mode="contained"
+        onPress={onPressSave}
+      >
+        保存
+      </Button>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    padding: 16
   }
 });
